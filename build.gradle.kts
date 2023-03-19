dde191bf93a2c46780db0e22ee6c4fcc14b27073
gunom <gu3062@naver.com>
2023-03-19 20:28:45 +0900

    [FEAT] dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.4"
+    id("org.springframework.boot") version "2.6.4"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
@@ -16,13 +16,31 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java:8.0.28")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("com.mysql:mysql-connector-j")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
+
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+    testImplementation("org.springframework.security:spring-security-test")
+
+    //jackson
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
+
+    api("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.redisson:redisson:3.16.3")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.3.50")
+    testImplementation("com.h2database:h2")}
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
